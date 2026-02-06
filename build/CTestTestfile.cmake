53ba5aef3327e5153d847cfede2870534d0afa4f
SudhanshuRai356 <sudhanshu3rai@gmail.com>
2026-02-06 17:04:08 +0530

    yes

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,10 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TestKem "/home/shura/projects/elefp/build/test_kem")
-set_tests_properties(TestKem PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;61;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
+set_tests_properties(TestKem PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;136;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
 add_test(CryptoUnit "/home/shura/projects/elefp/build/crypto_test")
-set_tests_properties(CryptoUnit PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;62;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
+set_tests_properties(CryptoUnit PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;137;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
+add_test(VpnComponents "/home/shura/projects/elefp/build/vpn_component_test")
+set_tests_properties(VpnComponents PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;138;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
+add_test(VpnIntegration "/home/shura/projects/elefp/build/vpn_integration_test")
+set_tests_properties(VpnIntegration PROPERTIES  _BACKTRACE_TRIPLES "/home/shura/projects/elefp/CMakeLists.txt;139;add_test;/home/shura/projects/elefp/CMakeLists.txt;0;")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,6 +40,42 @@ endif()
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/shura/projects/elefp/build/vpn_server")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_server")
+    endif()
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/shura/projects/elefp/build/vpn_client")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/vpn_client")
+    endif()
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/shura/projects/elefp/include/" FILES_MATCHING REGEX "/[^/]*\\.hpp$")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
